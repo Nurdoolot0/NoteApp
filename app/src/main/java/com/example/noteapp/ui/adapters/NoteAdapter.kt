58a07221bbf988d07bdf343944c694197162b27f
Nurdoolot0 <klopasd34@gmail.com>
2024-10-18 23:30:49 +0600

    commited

--- app/src/main/java/com/example/noteapp/ui/adapters/NoteAdapter.kt
+++ app/src/main/java/com/example/noteapp/ui/adapters/NoteAdapter.kt
@@ -8,15 +8,20 @@ import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
 import com.example.noteapp.data.models.NoteModel
 import com.example.noteapp.databinding.ItemNoteBinding
+import com.example.noteapp.ui.intetface.OnClickItem
 import java.text.SimpleDateFormat
 import java.util.Calendar
 import java.util.Locale
 
-class NoteAdapter : ListAdapter<NoteModel, NoteAdapter.ViewHolder>(DiffCallBack()) {
+class NoteAdapter(private val onLongClick: OnClickItem,
+                  private val onClick: OnClickItem) : ListAdapter<NoteModel, NoteAdapter.ViewHolder>(DiffCallBack()) {
+
     class ViewHolder(private val binding: ItemNoteBinding) : RecyclerView.ViewHolder(binding.root) {
         fun onBind(item: NoteModel) {
             binding.txtTitle.text = item.title
             binding.txtDescription.text = item.description
+
+            item.color?.let { binding.cardView.setCardBackgroundColor(it) }
             val calendar = Calendar.getInstance()
             val dateFormat = SimpleDateFormat("dd MMMM", Locale.getDefault())
             val timeFormat = SimpleDateFormat("HH:mm", Locale.getDefault())
@@ -39,6 +44,19 @@ class NoteAdapter : ListAdapter<NoteModel, NoteAdapter.ViewHolder>(DiffCallBack(
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
         holder.onBind(getItem(position))
+        holder.itemView.setOnLongClickListener {
+            onLongClick.onLongClick(getItem(position))
+            true
+        }
+        holder.itemView.setOnClickListener{
+            onClick.onClick(getItem(position))
+        }
+    }
+
+    fun updateItemColor(position: Int, color: Int) {
+        val item = getItem(position)
+        item.color = color
+        notifyItemChanged(position)
     }
 
     class DiffCallBack : DiffUtil.ItemCallback<NoteModel>() {
